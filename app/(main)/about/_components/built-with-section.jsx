761fcea9010a3594137a315a5548d0cae4366207
Sujay Das <sujay.das@outlook.in>
2025-07-12 22:22:46 +0530

    added a built-with section

--- app/(main)/about/_components/built-with-section.jsx
+++ app/(main)/about/_components/built-with-section.jsx
@@ -1,5 +1,4 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
 
 export default function BuiltWithSection() {
